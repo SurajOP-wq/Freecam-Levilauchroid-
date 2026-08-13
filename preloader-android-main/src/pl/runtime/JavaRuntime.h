0204744220e1113199b53351bf6bd56916d7c003
SurajOP-wq <snyadav.0905@gmail.com>
2026-08-14 02:05:14 +0530

    Revert "Fix vtable scanner safety and performance in FreeCam mod"

---
+++ preloader-android-main/src/pl/runtime/JavaRuntime.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <jni.h>
+
+namespace pl::runtime {
+
+void SetJavaVm(JavaVM *vm);
+JavaVM *GetJavaVm();
+
+void SetActivity(JNIEnv *env, jobject activity);
+void ClearActivity(JNIEnv *env);
+void CallActivityVoidMethod(const char *methodName);
+
+} // namespace pl::runtime
